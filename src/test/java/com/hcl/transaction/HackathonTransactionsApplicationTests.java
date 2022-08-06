cc9fc14890d2b123e5da324fc19ae5859babf8d5
Vikash Anand <vikash.anand.cse@gmail.com>
2022-08-06 12:48:10 +0400

    #initial structure

---
+++ src/test/java/com/hcl/transaction/HackathonTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
